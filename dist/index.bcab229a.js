988e2392d947501da8ee1ec61bca9ba9f516bdb7
rmaestrem <rmaestrem@uoc.edu>
2024-12-17 22:27:57 +0100

    Crear gitignore i afegir node_modules per a ignorar-ho

--- dist/index.bcab229a.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelectorAll(".temples-menu a");document.createElement("h1"),document.createElement("p"),console.log(e),e.addEventListener("click",e=>{preventDefault(),console.log(e)});
-//# sourceMappingURL=index.bcab229a.js.map
